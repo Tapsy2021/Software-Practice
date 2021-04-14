a93568d98011e5f305eced481e85df857f9883a0
Tapsy2021 <tapsmupazviriwo@gmail.com>
2021-04-14 20:35:23 +0200

    First Commit

---
+++ My First Project/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_First_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "Tapsy";
+            this.label1.Text = "this is my first project";
+
+            Console.Title = "Stop";
+            Console.BackgroundColor = ConsoleColor.Green;
+
+
+
+
+
+        }
+    }
+}
